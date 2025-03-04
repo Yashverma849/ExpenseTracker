2bd27b3329bc5c975cfb6659f622cb4734797a39
yashvermaconnect <112695392+Yashverma849@users.noreply.github.com>
2025-03-04 20:59:30 +0530

    login issue resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// filepath: /c:/Users/verma/expensetracker/next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/dashboard',
+          permanent: false,
+          has: [
+            {
+              type: 'cookie',
+              key: 'supabase-auth-token',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
